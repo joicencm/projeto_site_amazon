02a1a1bfe70cd41ef6f56c382dd7157c0902cf83
joicencm <joicencm@gmail.com>
2023-06-20 20:52:43 -0300

    inclusão do WebDriver e abrir navegador

--- src/test/java/drivers/DriversFactory.java
+++ src/test/java/drivers/DriversFactory.java
@@ -1,5 +1,9 @@
 package drivers;
 
+import org.openqa.selenium.WebDriver;
+
 public class DriversFactory {
+	
+	public static WebDriver driver;
 
 }
--- src/test/java/runner/Executa.java
+++ src/test/java/runner/Executa.java
@@ -1,5 +1,19 @@
 package runner;
 
-public class Executa {
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import drivers.DriversFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Executa extends DriversFactory {
+
+	public static void abrirNavegador() {
+
+		String browser = "https://www.amazon.com.br/";
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get(browser);
+		driver.manage().window().maximize();
+	}
 
 }
